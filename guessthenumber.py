1c4e0cd0b3a3fb511bace6cf82082f190df65239
bIgAnJ98 <163434957+bIgAnJ98@users.noreply.github.com>
2024-03-15 20:35:55 -0400

    Update guessthenumber.py
    Added a while loop to prompt the use to make an educated guess.
    This while loop will continuously prompt the user to make another guess if they guess a negative number or a number greater than selected x value.

--- guessthenumber.py
+++ guessthenumber.py
@@ -5,12 +5,24 @@ def guess_number(x):
     user_num = 0
     while user_num != random_num:
         user_num = int(input(f"Guess a number from 1 to {x}: "))
+        
+        ####
+        # This loop in the code will prompt the user to choose enter an int from 0 to x
+        # No negative numbers and no numbers greater than x
+        while user_num < 0 or user_num > x:
+            print ( f" Please enter a valid guess between 1 and {x}. " )
+            user_num = int(input(f"Guess a number from 1 to {x}: "))
+        else: 
+            break 
+        ####
+        
         if user_num < random_num:
             print("Not quite there yet. Number is low. Enter again: ")
         elif user_num > random_num:
             print("Hmm!. Number is high. Enter again: ")
     print(f"Yayy! You guessed right, the number is {random_num}")
 
+guess_number(3)
 
 
     
